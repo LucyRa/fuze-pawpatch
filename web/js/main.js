f795a3d8ced07ada419441854a1bfe8cd2450d72
LucyRa <lucyramplin@gmail.com>
2021-11-18 21:03:50 +0000

    Header

--- web/js/main.js
+++ web/js/main.js
@@ -4,8 +4,13 @@ $(function () {
         $('.nav-items').toggleClass('showNav');
     });
 
+    // Event handler to close cookies bar
+    $('.cookie-bar-control').on('click', () => {
+        $('.cookies-bar').addClass('display-none');
+    });
+
     // Event handler for 'Let's go walkies' buttons
-    $('.walkies-alert').on('click', () => {
-        alert('Page coming soon!');
+    $('.coming-soon').on('click', () => {
+        alert('Coming soon!');
     });
 });
